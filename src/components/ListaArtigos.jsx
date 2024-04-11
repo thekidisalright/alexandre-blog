4459da820e92e0013dfcfef9a18bc77e158dcf32
thekidisalright <dev.alexandresilva@gmail.com>
2024-04-11 05:22:15 -0300

    Update styles in index.css Update padding in .container class in App.module.css Update App.jsx to import artigos from ListaArtigos component Update favicon and theme color in index.html Add useState and Modal component, update Artigo component and styles fix package inicializar

---
+++ src/components/ListaArtigos.jsx
@@ -0,0 +1,179 @@
+const artigos = [
+  {
+    id: 1,
+    data: "09/04/2024",
+    titulo: "Quem sou eu?",
+    categoria: "Pessoal",
+    conteudo: (
+      <>
+        <p>
+          Meu nome é Alexandre, e sou apaixonado por desenvolvimento web. Nos
+          últimos anos, tenho dedicado tempo e esforço para explorar e aprimorar
+          minhas habilidades em programação e design. Desde uma idade precoce,
+          fui fascinado por tecnologia e arte, o que me levou a buscar
+          conhecimento e experiência nesse campo. Sou motivado pela busca
+          incessante pelo aprendizado e pela oportunidade de enfrentar desafios
+          que me permitam crescer e me desenvolver tanto pessoal quanto
+          profissionalmente. Acredito firmemente na importância de ter
+          criatividade e mesclar a arte com a tecnologia para criar ótimas
+          experiências para o usuário e estou comprometido em fazer uma
+          contribuição significativa nesse domínio.
+        </p>
+        <p>
+          Meu objetivo é continuar aprimorando minhas habilidades e conhecimento
+          em desenvolvimento web e design, para que eu possa contribuir para
+          projetos desafiadores e inovadores. Estou sempre em busca de novas
+          oportunidades de aprendizado e crescimento, e estou ansioso para
+          enfrentar os desafios que o futuro reserva.
+        </p>
+        <p>
+          Além do meu compromisso com minha área de atuação, sou também um
+          entusiasta fervoroso de filmes e música. Desde minha infância, os
+          filmes têm sido uma fonte de inspiração e entretenimento para mim.
+          Tenho um apreço particular por filmes que desafiam convenções e
+          provocam reflexões profundas sobre a condição humana. A música, por
+          sua vez, é uma parte essencial da minha vida, influenciando meu humor
+          e energia diária. Sou atraído por uma ampla gama de gêneros musicais,
+          mas tenho uma afinidade especial por artistas que incorporam letras
+          poéticas e melodias cativantes. Tanto nos filmes quanto na música,
+          encontro uma forma de expressão que transcende as barreiras culturais
+          e conecta as pessoas em um nível emocional profundo.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 2,
+    data: "09/04/2024",
+    titulo: "Top 10 filmes vistos em 2023",
+    categoria: "Filmes",
+    conteudo: (
+      <>
+        <p>
+          O ano de 2023 testemunhou uma infinidade de lançamentos
+          cinematográficos abrangendo diversos gêneros, temas e narrativas.
+          Enquanto as audiências navegavam por uma paisagem rica em ofertas
+          cinematográficas, alguns filmes se destacaram por sua narrativa
+          envolvente, técnicas inovadoras de produção cinematográfica e
+          ressonância emocional. Neste artigo, exploramos os 10 melhores filmes
+          assistidos em 2023, cada um contribuindo para a tapeçaria cultural do
+          ano e deixando um impacto duradouro nas audiências ao redor do mundo.
+        </p>
+        <ol>
+          <li>A Pequena Sereia (2023)</li>
+          <p>
+            Dirigido por Rob Marshall, "A Pequena Sereia" (2023) reimagina o
+            clássico amado da Disney com um toque moderno. Combinando visuais
+            deslumbrantes, números musicais encantadores e uma história
+            atemporal de amor e autodescoberta, o filme capturou os corações das
+            audiências de todas as idades. Através de sua vibrante representação
+            de maravilhas subaquáticas e temas universais de identidade e
+            aceitação, "A Pequena Sereia" emergiu como um filme marcante de
+            2023, encantando audiências e reacendendo a nostalgia pelo conto de
+            fadas querido.
+          </p>
+          <li>Entergalactic (2022)</li>
+          <p>
+            Dirigido por Dexter Navy e estrelado por Kid Cudi, "Entergalactic"
+            (2022) transcende as fronteiras tradicionais da narrativa para criar
+            uma experiência musical visualmente impressionante e emocionalmente
+            marcante. Mesclando elementos de ficção científica, fantasia e
+            música, o filme leva os espectadores a uma jornada surreal pela
+            psique de seu protagonista, explorando temas de saúde mental,
+            criatividade e existencialismo. Com sua abordagem inovadora à
+            narrativa e estética que desafia gêneros, "Entergalactic" cativou as
+            audiências e deixou uma impressão duradoura na paisagem
+            cinematográfica de 2023.
+          </p>
+          <li>Chevalier (2022)</li>
+          <p>
+            Dirigido por Athina Rachel Tsangari, "Chevalier" (2022) oferece uma
+            exploração satírica da masculinidade, competitividade e dinâmicas de
+            poder em meio ao cenário de um iate de luxo. Através de sua
+            sagacidade afiada, humor sutil e dinâmicas de personagens
+            perspicazes, o filme convida os espectadores a refletir sobre as
+            absurdidades do ego masculino e as expectativas sociais. Com suas
+            performances nuances e comentários provocativos, "Chevalier" emergiu
+            como um filme marcante de 2023, desafiando convenções e provocando
+            conversas sobre gênero e identidade.
+          </p>
+          <li>Antes do Amanhecer (1995)</li>
+          <p>
+            Dirigido por Richard Linklater, "Antes do Amanhecer" (1995)
+            permanece um clássico atemporal, cativando audiências com sua
+            representação íntima do amor, conexão e serendipidade. Ambientado
+            contra o pano de fundo de Viena, o filme segue dois estranhos que
+            embarcam em um romance espontâneo que se desenrola ao longo de uma
+            única noite. Através de seu diálogo naturalista e insights profundos
+            sobre relacionamentos humanos, "Antes do Amanhecer" continua a
+            ressoar com os espectadores, transcendendo barreiras geracionais e
+            reafirmando o poder duradouro do amor.
+          </p>
+          <li>Black Is King (2020)</li>
+          <p>
+            Dirigido por Beyoncé Knowles-Carter, "Black Is King" (2020) celebra
+            a riqueza da cultura e herança negra através de uma experiência
+            cinematográfica visualmente deslumbrante. Inspirado na música de "O
+            Rei Leão: O Presente", o filme reimagina a história icônica com
+            temas de empoderamento, identidade e autodescoberta. Através de seus
+            visuais impressionantes, narrativa poderosa e mensagem inspiradora,
+            "Black Is King" emergiu como um fenômeno cultural, ressoando com
+            audiências ao redor do mundo e reafirmando a beleza e resiliência da
+            experiência negra.
+          </p>
+          <li>A Chegada (2016)</li>
+          <p>
+            Dirigido por Denis Villeneuve, "A Chegada" (2016) transcende as
+            convenções da ficção científica tradicional para entregar uma
+            meditação provocativa sobre linguagem, comunicação e a natureza da
+            humanidade. Adaptado da história curta de Ted Chiang "História da
+            Sua Vida", o filme segue uma linguista encarregada de se comunicar
+            com visitantes extraterrestres cuja chegada provoca tensões globais.
+            Através de seu roteiro inteligente, cinematografia deslumbrante e
+            temas filosóficos profundos, "A Chegada" cativou audiências e ganhou
+            aclamação crítica por sua exploração de ideias e emoções complexas.
+          </p>
+          <li>Os Fabelmans (2022)</li>
+          <p>
+            Dirigido por Steven Spielberg, "Os Fabelmans" (2022) oferece uma
+            homenagem sincera à magia do cinema e ao poder da família. Inspirado
+            na infância de Spielberg, o filme celebra a alegria de contar
+            histórias, amizade e expressão criativa em meio à vida suburbana na
+            década de 1960. Com seu charme nostálgico, personagens cativantes e
+            reflexões comoventes sobre a inocência da infância, "Os Fabelmans"
+            ressoou com audiências de todas as idades, reafirmando o status de
+            Spielberg como um mestre contador de histórias.
+          </p>
+          <li>Um Lugar Chamado Notting Hill (1999)</li>
+          <p>
+            Dirigido por Roger Michell, "Um Lugar Chamado Notting Hill" (1999)
+            permanece uma comédia romântica querida por seu charme, humor e
+            química irresistível entre seus protagonistas, Hugh Grant e Julia
+            Roberts. Ambientado no vibrante bairro de Notting Hill em Londres, o
+            filme segue o romance improvável entre um dono de livraria e uma
+            atriz de Hollywood. Através de seu diálogo memorável, personagens
+            secundários inesquecíveis e mensagem atemporal de amor vencendo
+            tudo, "Um Lugar Chamado Notting Hill" continua a encantar audiências
+            e servir como uma comédia romântica quintessencial.
+          </p>
+          <li>Matilda: O Musical (2022)</li>
+          <p>
+            Dirigido por Matthew Warchus, "Matilda: O Musical" (2022) traz a
+            história amada de Roald Dahl à vida na tela grande com números
+            musicais deslumbrantes, visuais vibrantes e performances comoventes.
+            Centrado na precoce jovem heroína Matilda, o filme celebra o poder
+            da imaginação, resiliência e enfrentamento à injustiça. Com sua
+            energia contagiante, músicas memoráveis e temas universais de
+            empoderamento e bondade, "Matilda: O Musical" cativou audiências e
+            reacendeu o amor pela história atemporal de uma garota que ousa
+            desafiar as expectativas.
+          </p>
+          <li>Lady Bird (2017)</li>
+          <p>Dirigido por Greta Gerwig, "Lady Bird" (2017) oferece uma história comovente de amadurecimento que ressoa com autenticidade, humor e profundidade emocional. Ambientado em Sacramento, Califórnia, o filme segue o relacionamento turbulento entre uma adolescente espirituosa e sua mãe enquanto ela navega pelos desafios da adolescência e sonha com uma vida além de sua cidade natal. Através de seus personagens realistas, diálogos afiados e exploração sincera de identidade e pertencimento, "Lady Bird" emergiu como um sucesso crítico e comercial, recebendo elogios por sua representação honesta da experiência humana.</p>
+        </ol>
+      </>
+    ),
+  },
+];
+
+export { artigos };
